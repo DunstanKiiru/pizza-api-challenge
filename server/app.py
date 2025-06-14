937d7c87d8b34b888bb67ade4a99ce99fe743a8c
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-14 20:52:25 +0300

    updates app.py

--- server/app.py
+++ server/app.py
@@ -1,20 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .models import db
+from .config import Config
+from .models import restaurant, pizza, restaurant_pizza
 from .controllers.restaurant_controller import restaurant_bp
 from .controllers.pizza_controller import pizza_bp
 from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
-app = Flask(__name__)
-app.config.from_pyfile("config.py")
+db = SQLAlchemy()
+migrate = Migrate()
 
-db.init_app(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(restaurant_bp)
-app.register_blueprint(pizza_bp)
-app.register_blueprint(restaurant_pizza_bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-@app.route("/")
-def index():
-    return {"message": "Pizza Restaurant API is live!"}
+
+
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_pizza_bp)
+
+    return app
