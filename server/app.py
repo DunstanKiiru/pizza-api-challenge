88de2b584351a00af4007a6d3a612e49042aad09
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-14 20:49:10 +0300

    db definition in models/init.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db
-from controllers.restaurant_controller import restaurant_bp
-from controllers.pizza_controller import pizza_bp
-from controllers.restaurant_pizza_controller import restaurant_pizza_bp
+from .models import db
+from .controllers.restaurant_controller import restaurant_bp
+from .controllers.pizza_controller import pizza_bp
+from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
