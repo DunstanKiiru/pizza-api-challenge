05bb7f72dc9180ea846344bbbfbf0a0347b7effe
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-14 20:58:24 +0300

    updates app.py and config.py

--- server/app.py
+++ server/app.py
@@ -2,25 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
-from .models import restaurant, pizza, restaurant_pizza
+
+from .models import db
 from .controllers.restaurant_controller import restaurant_bp
 from .controllers.pizza_controller import pizza_bp
 from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
-
-db = SQLAlchemy()
-migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)
-
-
+    Migrate(app, db)
 
     app.register_blueprint(restaurant_bp)
     app.register_blueprint(pizza_bp)
     app.register_blueprint(restaurant_pizza_bp)
 
     return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ server/controllers/restaurant_pizza_controller.py
@@ -0,0 +1,41 @@
+from flask import Blueprint, request, jsonify
+from server.app import db
+from server.models.restaurant_pizza import RestaurantPizza
+from server.models.restaurant import Restaurant
+from server.models.pizza import Pizza
+
+restaurant_pizza_bp = Blueprint('restaurant_pizzas', __name__, url_prefix='/restaurant_pizzas')
+
+@restaurant_pizza_bp.route('', methods=['POST'])
+def create_restaurant_pizza():
+    try:
+        data = request.get_json()
+        price = data['price']
+        pizza_id = data['pizza_id']
+        restaurant_id = data['restaurant_id']
+
+        rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+        db.session.add(rp)
+        db.session.commit()
+
+        pizza = Pizza.query.get(pizza_id)
+        restaurant = Restaurant.query.get(restaurant_id)
+
+        return jsonify({
+            "id": rp.id,
+            "price": rp.price,
+            "pizza_id": pizza.id,
+            "restaurant_id": restaurant.id,
+            "pizza": {
+                "id": pizza.id,
+                "name": pizza.name,
+                "ingredients": pizza.ingredients
+            },
+            "restaurant": {
+                "id": restaurant.id,
+                "name": restaurant.name,
+                "address": restaurant.address
+            }
+        }), 201
+    except Exception as e:
+        return jsonify({"errors": [str(e)]}), 400
