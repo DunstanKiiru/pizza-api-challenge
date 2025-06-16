23ad196486a35f0b1bb785816221b976c4f62622
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-16 14:51:22 +0300

    defines home route and adds returning statement

--- server/app.py
+++ server/app.py
@@ -8,20 +8,17 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Environment variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Single db instance, initialized with app here
+# db instance
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Import models to register them with SQLAlchemy
 from server.models.restaurant import Restaurant
 from server.models.pizza import Pizza
 from server.models.restaurant_pizza import RestaurantPizza
 
-# Import blueprints and register them
 from server.controllers.restaurant_controller import restaurant_bp
 from server.controllers.pizza_controller import pizza_bp
 from server.controllers.restaurant_pizza_controller import restaurant_pizza_bp
@@ -32,7 +29,7 @@ app.register_blueprint(restaurant_pizza_bp)
 
 @app.route('/')
 def home():
-    return {"message": "Welcome to the Pizza Restaurant API!"}
+    return "Welcome to the Pizza Restaurant API!"
 
 if __name__ == '__main__':
     app.run(debug=True)
